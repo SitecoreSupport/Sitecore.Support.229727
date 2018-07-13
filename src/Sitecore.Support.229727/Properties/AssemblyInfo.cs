3af4ea56fa64149ad403413ecbb18e6c899bf63c
DK\AMCH <amch@sitecore.net>
2018-07-13 08:27:54 +0300

    LoginClicked method was modified in the child class to catch the "The method or operation is not implemented." exception

---
+++ src/Sitecore.Support.229727/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.229727")]
+[assembly: AssemblyProduct("Sitecore.Support.229727")]
+[assembly: ComVisible(false)]
